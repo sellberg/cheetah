a0260d983a8368ee821533d4d142f1a3b50b2816
Jonas Sellberg <sellberg@slac.stanford.edu>
2011-06-23 02:08:04 -0700

    Added python viewer for energy histograms, fixed a bug in viewSAXS.py

--- pythonscripts/viewEnergyHistograms.py
+++ pythonscripts/viewEnergyHistograms.py
@@ -2,9 +2,9 @@
 
 # Usage:
 # In this directory, type:
-#    ./viewSAXS.py -rxxxx -m 400 (optional) -x 800 (optional)
+#    ./viewEnergyHistograms.py -rxxxx -m 400 (optional) -x 800 (optional)
 # For details, type 
-#	 python viewSAXS.py --help
+#	 python viewEnergyHistograms.py --help
 # where rxxxx is the run number of hits and nonhits found using the hitfinder executable. 
 # By default, this script looks into the h5 files that are in the appropriate rxxxx directory
 #
@@ -18,10 +18,6 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-r", "--run", action="store", type="string", dest="runNumber", 
 					help="run number you wish to view", metavar="rxxxx", default="")
-parser.add_option("-m", "--min", action="store", type="float", dest="min_value", 
-					help="ignore intensities below this q-value", metavar="min_value", default="0")
-parser.add_option("-x", "--max", action="store", type="float", dest="max_value", 
-					help="ignore intensities above this q-value", default="100000")
 
 (options, args) = parser.parse_args()
 
@@ -42,26 +38,33 @@ source_dir = "/reg/d/psdm/cxi/cxi25410/scratch/cleaned_hdf5/"
 write_dir = ""
 
 runtag = "r%s"%(options.runNumber)
-print source_dir+runtag+"/"+runtag+"-SAXS.h5"
-f = H.File(source_dir+runtag+"/"+runtag+"-SAXS.h5","r")
+print source_dir+runtag+"/"+runtag+"-energy_histograms.h5"
+f = H.File(source_dir+runtag+"/"+runtag+"-energy_histograms.h5","r")
 d = N.array(f['/data/data'])
-q = d[0]
-saxs = d[1]
+energies = d[0]
+xe = d[1]
+wavelengths = d[2]
+xw = d[3]
 f.close()
 
-q[N.isnan(saxs)] = 0
-q *= (q > options.min_value)
-q *= (q < options.max_value)
-q = q[q != 0]
-saxs = saxs[q != 0]
+energies[N.isnan(energies)] = 0
+wavelengths[N.isnan(wavelengths)] = 0
 
 print "Interactive controls for zooming at the bottom of figure screen (zooming..etc)."
 print "Hit Ctl-\ or close all windows (Alt-F4) to terminate viewing program."
 
 fig = P.figure()
-P.plot(q, saxs)
+P.plot(xe, energies)
 canvas = fig.add_subplot(111)
-canvas.set_title(runtag + "_SAXS")
-P.xlabel("Q")
-P.ylabel("I(Q)")
+canvas.set_title(runtag + "_energies")
+P.xlabel("E (eV)")
+P.ylabel("events")
+P.draw()
+
+fig = P.figure()
+P.plot(xw, wavelengths)
+canvas = fig.add_subplot(111)
+canvas.set_title(runtag + "_wavelengths")
+P.xlabel("lambda (A)")
+P.ylabel("events")
 P.show()
