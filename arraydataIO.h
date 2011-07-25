db6d1757991db81802ab8474e6e0b507908fb65f
feldkamp <feldkamp@gmx.com>
2011-07-25 11:44:42 -0700

    changed preprocessor definitions

--- arraydataIO.h
+++ arraydataIO.h
@@ -13,48 +13,51 @@
 //	INFO: if none of the i/o is actually needed, you can undefine all of the macros
 //
 
+	#define ARRAYDATAIO_EDF				//use EDF
+	#define ARRAYDATAIO_TIFF			//use TIFF
+	#define ARRAYDATAIO_HDF5			//use HDF5
+
+//	#undef ARRAYDATAIO_EDF				//do not use EDF
+//	#undef ARRAYDATAIO_TIFF				//do not use TIFF
+//	#undef ARRAYDATAIO_HDF5				//do not use HDF5
+
+#ifdef CHEETAH
+	#undef ARRAYDATAIO_EDF				//do not use EDF in cheetah	
+#endif
+
+
 #ifndef _arraydataIO_h
 #define _arraydataIO_h
 
-#include <string>
-#include "arrayclasses.h"
+	#include <string>
+	#include "arrayclasses.h"
 
-class arraydataIO{
+	class arraydataIO{
 
+	public:
+		arraydataIO();
+		~arraydataIO();
 
-//#define ARRAYDATAIO_EDF			//use EDF
-#undef ARRAYDATAIO_EDF				//do not use EDF
-
-#define ARRAYDATAIO_TIFF			//use TIFF
-//#undef ARRAYDATAIO_TIFF				//do not use TIFF
-
-#define ARRAYDATAIO_HDF5			//use HDF5
-//#undef ARRAYDATAIO_HDF5			//do not use HDF5
-
-public:
-	arraydataIO();
-	~arraydataIO();
-
-	//------------------------------------------------------------------------------EDF
-	int readFromEDF( std::string filename, array2D *&dest ) const;										// works
-	int writeToEDF( std::string filename, array2D *src ) const;											// works
-	
-	//------------------------------------------------------------------------------Tiff	
-	int readFromTiff( std::string filename, array2D *&dest ) const;										// works, exept for a scaling factor --> needs fix
-	
-	// scaleFlag = 0 --> do not scale data
-	// scaleFlag = 1 --> scale data to 0..65535
-	int writeToTiff( std::string filename, array2D *src, int scaleFlag = 0, int verbose = 0 ) const;    // works
-	
-	
-	//------------------------------------------------------------------------------HDF5
-	int readFromHDF5( std::string filename, array2D *&dest ) const;										// seems to work, still testing, use with caution
-	
-	// dataType = 0 --> write as doubles (default) 
-	// dataType = 1 --> write as float
-	// dataType = 2 --> write as int
-	int writeToHDF5( std::string filename, array2D *src, int dataType = 0, int verbose = 0 ) const;		// works
-};
+		//------------------------------------------------------------------------------EDF
+		int readFromEDF( std::string filename, array2D *&dest ) const;										// works
+		int writeToEDF( std::string filename, array2D *src ) const;											// works
+		
+		//------------------------------------------------------------------------------Tiff	
+		int readFromTiff( std::string filename, array2D *&dest ) const;										// works, exept for a scaling factor --> needs fix
+		
+		// scaleFlag = 0 --> do not scale data
+		// scaleFlag = 1 --> scale data to 0..65535
+		int writeToTiff( std::string filename, array2D *src, int scaleFlag = 0, int verbose = 0 ) const;    // works
+		
+		
+		//------------------------------------------------------------------------------HDF5
+		int readFromHDF5( std::string filename, array2D *&dest ) const;										// seems to work, still testing, use with caution
+		
+		// dataType = 0 --> write as doubles (default) 
+		// dataType = 1 --> write as float
+		// dataType = 2 --> write as int
+		int writeToHDF5( std::string filename, array2D *src, int dataType = 0, int verbose = 0 ) const;		// works
+	};
 
 
 #endif
