e10ab3a10f53636b290900ada12822c3be1a1735
Jonas Sellberg <sellberg@slac.stanford.edu>
2011-07-27 17:56:52 -0700

    Changed terminal output for processed events if useAutoHotpixel is activated Added case llcp to upload script

--- worker.cpp
+++ worker.cpp
@@ -288,16 +288,31 @@ void *worker(void *threadarg) {
 			writeHDF5(threadInfo, global, eventname, global->backgroundfinder.cleanedfp);
 		}
 	}
-	if (global->icefinder.use) {
-		printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.ice, threadInfo->nPeaks);
-	} else if (global->waterfinder.use) {
-		printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.water, threadInfo->nPeaks);
-	} else if (global->hitfinder.use) {
-		printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.standard, threadInfo->nPeaks);
-	} else if ((global->nprocessedframes % 100) == 0) {
-		printf("r%04u:%i (%3.1f Hz): Processed %i events\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, global->nprocessedframes);
+	
+	
+	/*
+	 *	Write out diagnostics to screen
+	 */	
+	if (global->useAutoHotpixel) {
+		if (global->icefinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i, hot=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.ice, threadInfo->nPeaks, threadInfo->nHot);
+		} else if (global->waterfinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i, hot=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.water, threadInfo->nPeaks, threadInfo->nHot);
+		} else if (global->hitfinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i, hot=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.standard, threadInfo->nPeaks, threadInfo->nHot);
+		}
+	} else {
+		if (global->icefinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.ice, threadInfo->nPeaks);
+		} else if (global->waterfinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.water, threadInfo->nPeaks);
+		} else if (global->hitfinder.use) {
+			printf("r%04u:%i (%3.1f Hz): Processed (hit=%i, nat/npeaks=%i)\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, hit.standard, threadInfo->nPeaks);
+		} else if ((global->nprocessedframes % 100) == 0) {
+			printf("r%04u:%i (%3.1f Hz): Processed %i events\n", (int)global->runNumber, (int)threadInfo->threadNum, global->datarate, global->nprocessedframes);
+		}
 	}
-
+	
 	/*
 	 *	Write out information on each frame to a log file
 	 */
